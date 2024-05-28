77c6aab85502f3d726ab149dda9c8f804b3d73a7
jd3545 <79057591+jd3545@users.noreply.github.com>
2024-05-28 03:09:17 -0400

    build ready

--- app/api/assistant.ts
+++ app/api/assistant.ts
@@ -1,7 +1,7 @@
 import { AssistantResponse } from "ai";
 import OpenAI from "openai";
 import { NextRequest } from "next/server";
-import { z } from "zod";
+import { string, z } from "zod";
 import { zfd } from "zod-form-data";
 
 const schema = zfd.formData({
@@ -9,7 +9,13 @@ const schema = zfd.formData({
 	message: zfd.text(),
 	file: z.instanceof(Blob)
 });
-
+const homeTemperatures = {
+  bedroom: 20,
+  'home office': 21,
+  'living room': 21,
+  kitchen: 22,
+  bathroom: 23,
+};
 // Create an OpenAI API client (that's edge friendly!)
 const openai = new OpenAI({
 	apiKey: process.env.OPENAI_API_KEY || ""
@@ -64,7 +70,48 @@ export async function POST(req: NextRequest) {
       while (
         runResult?.status === 'requires_action' &&
         runResult.required_action?.type === 'submit_tool_outputs'
-      )
+      ) {
+        const tool_outputs =
+          runResult.required_action.submit_tool_outputs.tool_calls.map(
+            (toolCall: any) => {
+              const parameters = JSON.parse(toolCall.function.arguments);
+
+              switch (toolCall.function.name) {
+                case 'getRoomTemperature': {
+                  const temperature =
+                    homeTemperatures[
+                      parameters.room as keyof typeof homeTemperatures
+                    ];
+
+                  return {
+                    tool_call_id: toolCall.id,
+                    output: temperature.toString(),
+                  };
+                }
+
+                case 'setRoomTemperature': {
+                  const oldTemperature =
+                    homeTemperatures[
+                      parameters.room as keyof typeof homeTemperatures
+                    ];
+
+                  homeTemperatures[
+                    parameters.room as keyof typeof homeTemperatures
+                  ] = parameters.temperature;
+
+                  return {
+                    tool_call_id: toolCall.id,
+                    output: `temperature set successfully`,
+                  };
+                }
+
+                default:
+                  throw new Error(
+                    `Unknown tool call function: ${toolCall.function.name}`,
+                  );
+              }
+            },
+          );
         runResult = await forwardStream(
           openai.beta.threads.runs.submitToolOutputsStream(
             threadId,
@@ -73,6 +120,7 @@ export async function POST(req: NextRequest) {
            { signal: req.signal },
           ),
         );
-		}
+      }
+		},
 	);
 }
